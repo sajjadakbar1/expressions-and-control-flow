4a7a22f6249504f078821eaa6a6c31d7d96bd8d5
Muhammad Sajjad Akbar <makbar@ibs-b.hu>
2023-03-09 02:56:50 +0500

    file added

---
+++ print_bigger.py
@@ -0,0 +1,6 @@
+# Write a program that asks for two numbers and prints the bigger one
+
+a = input("Enter your first number: ")
+b = input("Enter your second number: ")
+if a > b: print(a+" is bigger than "+b)
+if a < b: print(a+" is smaller than "+b)

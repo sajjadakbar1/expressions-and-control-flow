6b43fe8bd998777376f5406c5410213b38e9dc2a
Muhammad Sajjad Akbar <makbar@ibs-b.hu>
2023-03-09 03:47:07 +0500

    file added

---
+++ conditional_variable_mutation.py
@@ -0,0 +1,49 @@
+a = 24
+output1 = 0
+# if a is even increment output1 by one
+
+
+print(output1)
+
+
+
+
+b = 13
+output2 = ""
+# if b is between 10 and 20 set output2 to "Sweet!"
+# if less than 10 set output2 to "Less!",
+# if more than 20 set output2 to "More!"
+
+
+print(output2)
+
+
+
+c = 123
+credits = 100
+is_bonus = False
+# if credits are at least 50,
+# and is_bonus is false decrement c by 2
+# if credits are smaller than 50,
+# and is_bonus is false decrement c by 1
+# if is_bonus is true c should remain the same
+
+
+print(c)
+
+
+
+
+d = 8
+time = 120
+output3 = ""
+# if d is dividable by 4
+# and time is not mor
+e than 200
+# set output3 to "check"
+# if time is more than 200
+# set output3 to "Time out"
+# otherwise set output3 to "Run Forest Run!"
+
+
+print(output3)

5e86a91e53719ea739160be69c708dd7d3df7696
Muhammad Sajjad Akbar <makbar@ibs-b.hu>
2023-03-10 01:49:15 +0500

    file added

---
+++ i_wont_cheat_on_the_exams.py
@@ -0,0 +1,9 @@
+# Create a program that writes this line 100 times:
+# "I won't cheat on the exam!"
+
+var = "I won't cheat on the exam!"
+
+count = 0
+while count < 100:
+    print(var)
+    count += 1

ca69c0ab5f65c19a4175e00afd93ff793f8c50a0
Muhammad Sajjad Akbar <makbar@ibs-b.hu>
2023-03-10 22:25:38 +0500

    code fixed

--- average_of_input.py
+++ average_of_input.py
@@ -5,29 +5,21 @@
 #
 # Sum: 22, Average: 4.4
 
-# creating an empty list
+
 lst = []
-  
-# number of elements as input
 n = int(input("Enter total numbers first, than enter numbers one by one by pressing enter: "))
   
-# iterating till the range
 for i in range(0, n):
     ele = int(input())
   
-    lst.append(ele) # adding the element
-      
-# start parameter is not provided
+    lst.append(ele)
 Sum = str(sum(lst))
+
 print("Your entered numbers: "+str(lst))
 print('\n'"Sum of your entered numbers are: "+Sum)
 
-# To get average of a list
 def Average(lst):
     return sum(lst) / len(lst)
- 
-# Driver Code
+
 average = Average(lst)
- 
-# Printing average of the list
 print("Average of your entered numbers is: ", round(average, 2))

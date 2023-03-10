ba1380d7939de0c0c8c4f10ab667265ab793b518
Muhammad Sajjad Akbar <makbar@ibs-b.hu>
2023-03-10 21:16:26 +0500

    code fixed

--- average_of_input.py
+++ average_of_input.py
@@ -1,5 +1,7 @@
-# Write a program that asks for 5 integers in a row,
-# then it should print the sum and the average of these numbers like:
+# Write a program that asks for a number
+# It would ask this many times to enter an integer
+# If all the integers are entered, it should print the sum and average of these
+# integers like:
 #
 # Sum: 22, Average: 4.4
 
@@ -7,7 +9,7 @@
 lst = []
   
 # number of elements as input
-n = int(input("Enter total numbers you want: "))
+n = int(input("Enter total numbers first, than enter numbers one by one by pressing enter: "))
   
 # iterating till the range
 for i in range(0, n):
@@ -17,7 +19,7 @@ for i in range(0, n):
       
 # start parameter is not provided
 Sum = str(sum(lst))
-print("You entered numbers: "+str(lst))
+print("Your entered numbers: "+str(lst))
 print('\n'"Sum of your entered numbers are: "+Sum)
 
 # To get average of a list

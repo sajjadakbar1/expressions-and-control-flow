650adbc812067486807ef71cb95138b20e5e1071
Muhammad Sajjad Akbar <makbar@ibs-b.hu>
2023-03-10 22:05:46 +0500

    file added

---
+++ draw_chess_table.py
@@ -0,0 +1,16 @@
+# Crate a program that draws a chess table like this
+#
+# % % % % 
+#  % % % %
+# % % % %
+#  % % % %
+# % % % %
+#  % % % %
+# % % % % 
+#  % % % %
+#
+
+number = '%'    
+
+for s in range(9,18):    
+ print(number * s)
